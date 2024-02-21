8af2697350928067ef164bb08c238cc031fd3582
Hélio Júnior <heliorpjunior116@gmail.com>
2024-02-21 12:05:43 -0300

    feat: criação inicial de api

--- index.js
+++ index.js
@@ -9,4 +9,28 @@ app.get("/oi", function (req, res) {
   res.send("Olá mundo!");
 });
 
+const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"];
+
+app.get("/item", function (req, res) {
+  res.json(lista);
+});
+
+app.get("/item/:id", function (req, res) {
+  const { id } = req.params;
+
+  const item = lista[id];
+
+  res.json(item);
+});
+
+app.use(express.json());
+
+app.post("/item", function (req, res) {
+  const { nome } = req.body;
+
+  lista.push(nome);
+
+  res.status(201).json({ mensagem: "Item adicionado com sucesso!" });
+});
+
 app.listen(3000);
